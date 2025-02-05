8a0636d44be361cadea9a84fd8e2b8569cc878cf
sims-kale <simskale122@gmail.com>
2025-02-05 12:56:19 +0000

    Add obstacle avoidance functionality

--- controllers/epunk_go_forward/epunk_go_forward.py
+++ controllers/epunk_go_forward/epunk_go_forward.py
@@ -1,20 +1,22 @@
-"""epunk_go_forward controller."""
-
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
 from controller import Robot, Motor
+
 TIME_STEP = 64
-max_speed = 3.89
+
+MAX_SPEED = 6.28
 
 # create the Robot instance.
 robot = Robot()
 
+# get a handler to the motors and set target position to infinity (speed control)
 leftMotor = robot.getDevice('left wheel motor')
 rightMotor = robot.getDevice('right wheel motor')
 leftMotor.setPosition(float('inf'))
 rightMotor.setPosition(float('inf'))
-# rightMotor.setPosition(10.0)
-leftMotor.setVelocity(0.1*max_speed)
-rightMotor.setVelocity(0.1*max_speed)
-while robot.step(TIME_STEP) != 0:
-    pass
+
+
+# set up the motor speeds at 10% of the MAX_SPEED.
+leftMotor.setVelocity(0.1 * MAX_SPEED)
+rightMotor.setVelocity(0.1 * MAX_SPEED)
+
+while robot.step(TIME_STEP) != -1:
+   pass
