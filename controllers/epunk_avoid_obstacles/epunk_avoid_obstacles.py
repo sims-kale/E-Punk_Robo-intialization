261f64bd998bee7487cdf6fc4a99ed0c12e51622
sims-kale <simskale122@gmail.com>
2025-04-10 04:00:34 +0100

    update

--- controllers/epunk_avoid_obstacles/epunk_avoid_obstacles.py
+++ controllers/epunk_avoid_obstacles/epunk_avoid_obstacles.py
@@ -1,8 +1,9 @@
-"""epunk_avoid_obstacles controller."""
+"""e-punk_avoid_obstacles controller."""
 
-from controller import Robot, Motor
+from controller import Robot
+
 TIME_STEP = 64
-max_speed = 3.89
+MAX_SPEED = 3.89
 
 # create the Robot instance.
 robot = Robot()
@@ -24,24 +25,23 @@ rightMotor.setPosition(float('inf'))
 leftMotor.setVelocity(0.0)
 rightMotor.setVelocity(0.0)
 
-
-while robot.step(TIME_STEP) != 0:
+while robot.step(TIME_STEP) != -1:
     psValues = []
     for i in range(8):
         psValues.append(ps[i].getValue())
-        
+
     right_obstacle = psValues[0] > 80.0 or psValues[1] > 80.0 or psValues[2] > 80.0
     left_obstacle = psValues[5] > 80.0 or psValues[6] > 80.0 or psValues[7] > 80.0
-     # initialize motor speeds at 50% of MAX_SPEED.
-    leftSpeed  = 0.5 * MAX_SPEED
+    # initialize motor speeds at 50% of MAX_SPEED.
+    leftSpeed = 0.5 * MAX_SPEED
     rightSpeed = 0.5 * MAX_SPEED
     if left_obstacle:
         # turn right
-        leftSpeed  = 0.5 * MAX_SPEED
+        leftSpeed = 0.5 * MAX_SPEED
         rightSpeed = -0.5 * MAX_SPEED
     elif right_obstacle:
         # turn left
-        leftSpeed  = -0.5 * MAX_SPEED
+        leftSpeed = -0.5 * MAX_SPEED
         rightSpeed = 0.5 * MAX_SPEED
     # write actuators inputs
     leftMotor.setVelocity(leftSpeed)
